858da5eb29ebad84c935d52aed1fac109aa01b68
AhmdeAbid <ahmed.abid@triazur.com>
2022-10-28 17:51:07 +0200

    Wpf

---
+++ cloudDB/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Windows;
+using Autodesk.Revit.Creation;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Document = Autodesk.Revit.DB.Document;
+
+namespace cloudDB
+{ 
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public UIDocument uidoc { get; } 
+        public Document doc { get; }
+        public MainWindow(UIDocument UiDoc)
+        {
+            uidoc = UiDoc;
+            doc = UiDoc.Document;
+            InitializeComponent();
+            Title = "TEST FOR REVIT INTEGRATION";
+        }
+
+    }
+}
