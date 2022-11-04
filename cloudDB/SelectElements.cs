533ce12fbbd4bf6690cb621832f3f7ef9b1b1c72
ahmed.abid@tridyme.com <ahmed.abid@tridyme.com>
2022-11-04 10:47:56 +0100

    Save

--- cloudDB/SelectElements.cs
+++ cloudDB/SelectElements.cs
@@ -6,22 +6,52 @@ using System.Threading.Tasks;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.DB;
 using System.Windows.Forms;
-
-
+using Autodesk.Revit.UI.Selection;
 
 namespace cloudDB
 {
-    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.ReadOnly)]
     public class SelectElements : IExternalCommand
     {
-
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData,
+            ref string message, ElementSet elements)
         {
+            try
+            {
+                // Select some elements in Revit before invoking this command
 
-            MessageBox.Show("Hello World");
+                // Get the handle of current document.
+                UIDocument uidoc = commandData.Application.ActiveUIDocument;
 
-            return Result.Succeeded;
+                // Get the element selection of current document.
+                Selection selection = uidoc.Selection;
+                ICollection<ElementId> selectedIds = uidoc.Selection.GetElementIds();
+
+                if (0 == selectedIds.Count)
+                {
+                    // If no elements selected.
+                    TaskDialog.Show("Revit", "You haven't selected any elements.");
+                }
+                else
+                {
+                    String info = "Ids of selected elements in the document are: ";
+                    foreach (ElementId id in selectedIds)
+                    {
+                        info += "\n\t" + id.IntegerValue;
+                    }
+
+                    TaskDialog.Show("Revit", info);
+                }
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Autodesk.Revit.UI.Result.Failed;
+            }
+
+            return Autodesk.Revit.UI.Result.Succeeded;
         }
+        /// </ExampleMethod>
     }
+
 }
